8a361d2a0ac48d89c7e707c1ecc4d17be95e49fa
drewhosick <drewhosick@gmail.com>
2021-10-02 20:36:50 -0400

    Completed chat application code

--- scripts/chat.js
+++ scripts/chat.js
@@ -12,6 +12,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat(message){
         // format a chat object
@@ -26,10 +27,29 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback){
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if(change.type === 'added'){
+                        // update the ui
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+    updateName(username){
+        this.username = username;
+        localStorage.setItem('username', username);
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsub){
+            this.unsub();
+        }
+    }
 }
 
-const chatroom = new Chatroom('gaming', 'shaun');
-
-chatroom.addChat('hello everyone')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err));
